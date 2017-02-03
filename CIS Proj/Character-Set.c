75d3559a5fcb401aeaa453925b45d20353c13790
doubleomidge <doubleomidge@gmail.com>
2017-02-03 09:51:41 -0700

    Update Character-Set.c
    I fixed the compiling errors, I was missing the & before the integers. (They are unnecessary with char arrays)

--- CIS Proj/Character-Set.c
+++ CIS Proj/Character-Set.c
@@ -1,23 +1,14 @@
 #include <stdio.h>
 
-/* Parts we need
-        Create Character
-        Load/Write New Character
-        Random Dice
-        How much damage is dealt
-        Write damage to character
-        */
-
 struct CharacterSet{
     char name[12];
     char race[12];
     int attack;
     int defense;
-}
+};
 
 void CreateCharacter(){
     struct CharacterSet aCharacter;
-    int quotient = dividend/divisor;
     
     printf("Enter character's name: ");
         scanf("%s", aCharacter.name);
@@ -26,12 +17,12 @@ void CreateCharacter(){
         scanf("%s", aCharacter.race);
     
     printf("Enter attack: ");
-        scanf("%lf", aCharacter.attack);
+        scanf("%d", &aCharacter.attack);
         
     printf("Enter defense: ");
-        scanf("%lf", aCharacter.defense);
+        scanf("%d", &aCharacter.defense);
     
-    printf("%s is %s and has a strength of %d and health of %d o\n", aCharacter.name, aCharacter.race, aCharacter.attack, aCharacter.defense);
+    printf("%s is %s and has an attack of %d and a defense of %d.\n", aCharacter.name, aCharacter.race, aCharacter.attack, aCharacter.defense);
 }
 
 int main(){
