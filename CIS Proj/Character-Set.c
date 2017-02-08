14189bf0426e47157e7ef33b7f0e66419cebff35
doubleomidge <doubleomidge@gmail.com>
2017-02-08 09:42:04 -0700

    Update Character-Set.c
    With the condition that attack and defense cannot be added to be greater than 10

--- CIS Proj/Character-Set.c
+++ CIS Proj/Character-Set.c
@@ -7,7 +7,8 @@ struct CharacterSet{
     int defense;
 };
 
-void CreateCharacter(){
+/* return character set */
+struct CharacterSet createCharacter(){
     struct CharacterSet aCharacter;
     
     printf("Enter character's name: ");
@@ -16,16 +17,35 @@ void CreateCharacter(){
     printf("Enter charactere's race: ");
         scanf("%s", aCharacter.race);
     
+    printf("Now choose the strength of our character. Your attack and defense score cannot be greater than 10.\n");
+    
     printf("Enter attack: ");
         scanf("%d", &aCharacter.attack);
         
     printf("Enter defense: ");
-        scanf("%d", &aCharacter.defense);
+        scanf("%d", &aCharacter.defense); 
     
     printf("%s is %s and has an attack of %d and a defense of %d.\n", aCharacter.name, aCharacter.race, aCharacter.attack, aCharacter.defense);
+
+    return aCharacter;
 }
 
 int main(){
-    CreateCharacter();
+    struct CharacterSet aCharacter = createCharacter();
+
+    int sum = -5;
+    while(sum <= 10){
+        sum = aCharacter.attack + aCharacter.defense;
+
+        printf("Oops. There’s a problem. The sum of your attack and defense points is greater than 10. Please try again.\n");
+        
+        printf("Enter attack: ");
+            scanf("%d", &aCharacter.attack);
+        
+        printf("Enter defense: ");
+            scanf("%d", &aCharacter.defense); 
+    
+        printf("%s is %s and has an attack of %d and a defense of %d.\n", aCharacter.name, aCharacter.race, aCharacter.attack, aCharacter.defense);
+    }
     return 0;
 }
