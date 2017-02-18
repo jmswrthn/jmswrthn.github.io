ecf45cddc8c3b4791b030f6f9f62e47e9a3fa607
James Worthen <wor11012@byui.edu>
2017-02-17 18:00:06 -0700

    halfway

--- linklistjs.js
+++ linklistjs.js
@@ -2,6 +2,7 @@
     document.getElementById("linklist").onchange = function() {
     if (this.selectedIndex!==0) {
         window.location.href = this.value;
+        this.selectedIndex=0;
     }
 };
 </script>
