bee30687bf57139d4002c6effce6df0b0016ac1e
James Worthen <wor11012@byui.edu>
2017-03-31 14:56:33 -0600

    stage 1

---
+++ project/scripts/modules.js
@@ -0,0 +1,5 @@
+$(function () {
+    $("#page-header").load("/project/modules/header.html");
+    $("#page-nav").load("/project/modules/navigation.html");
+    $("#footer-content").load("/project/modules/footer.html");
+});
